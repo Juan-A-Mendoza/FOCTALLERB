e6605e10a789e734922cb44891ffd10df879b3bb
Juan-A-Mendoza <juan.a.m.b.9@gmail.com>
2026-05-09 15:43:03 -0400

    sexo commit

--- inventario/views.py
+++ inventario/views.py
@@ -1,22 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .model import Producto
+from .models import Producto
 from .forms import ProductoForm
 
 def lista_productos(request):
-    productos = Producto.objects.all()
-    return render(request, 'inventario/lista.html', {'productos': productos})
-    @login_required # Proteccion nativa
-
-    def crear_producto(request):
-        if request.method == 'POST':
-            form = ProductoForm(request.POST)
-            if form.is_valid():
-                form.save()
-            return redirect("lista_productos")
-            else:
-                form = ProductoForm()
-            return render(request, 'inventario/formulario.html',{'form':form})  
+pathroductos = producto.objects.all()
+return render(request, 'inventario/lista.html', {'productos': productos})
+@login_required  # Protección nativa (Pág. 9 presentación)
+def crear_producto(request):
+	if request.method == 'POST':
+	form = ProductoForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('lista_productos')
+	else:
+		form = ProductoForm()
+	return render(request, 'inventario/formulario.html', {'form': form})
--- sistema_foc/urls.py
+++ sistema_foc/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inventario.views import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.lista_productos, name='lista_productos')
 ]
